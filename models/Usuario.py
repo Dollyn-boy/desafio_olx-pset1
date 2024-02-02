9619b69fb1bca26eb9556796b1b6d62dbfc85e2b
Vitor Dórea <120328225+Dollyn-boy@users.noreply.github.com>
2024-02-02 20:59:24 -0300

    main-update

--- models/Usuario.py
+++ models/Usuario.py
@@ -6,13 +6,22 @@ class Usuario:
         self.user = user
         self.telefone = telefone
         self.email = email
-        self.senha = senha
+        self.__senha = senha
         self.enderecos = []
+    
+    @property 
+    def senha(self):
+        return self.__senha
+    
+    @senha.setter
+    def senha(self, nova_senha):
+        self.__senha = nova_senha
     
     def inserir_enderoco(self, cep):
         self.enderecos.append(Endereco(cep))
     
     def lista_enderecos(self):
+        print(self.nome)
         for endereco in self.enderecos:
             print(f"{endereco.cidade}, {endereco.uf}, {endereco.bairro}")
 
---
+++ models/main.py
@@ -0,0 +1,6 @@
+from Usuario import Usuario
+from Usuario import Endereco 
+
+user1 = Usuario("Vitor", "vitordoreahdx11@gmail.com", "DomVital", 89028922, "japodaomossar123")
+user1.inserir_enderoco("4460000")
+user1.lista_enderecos()
