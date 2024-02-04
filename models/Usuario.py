412b3957adb1018a6c6df7d071db0cda7f83c388
Vitor Dórea <120328225+Dollyn-boy@users.noreply.github.com>
2024-02-04 10:29:21 -0300

    new-input

--- models/Usuario.py
+++ models/Usuario.py
@@ -1,13 +1,27 @@
-import requests
+from Enderecos import Endereco
+from Produto import Produto
+from abc import ABC, abstractmethod
 
+class ComportamentoDoUsuario:
+    @abstractmethod
+    def executar(self, usuario):
+        pass
+
+class ComportamenVendedor(ComportamentoDoUsuario):
+    def executar(self, usuario):
+        usuario.produtos_cadastrados = [] 
+
+class Comp
+        
 class Usuario:
-    def __init__(self, nome, email, user, telefone, senha):
+    def __init__(self, nome, email, user, telefone, senha, comportamento):
         self.nome = nome
         self.user = user
         self.telefone = telefone
         self.email = email
         self.__senha = senha
         self.enderecos = []
+        self.comportamento = comportamento
     
     @property 
     def senha(self):
@@ -24,17 +38,19 @@ class Usuario:
         print(self.nome)
         for endereco in self.enderecos:
             print(f"{endereco.cidade}, {endereco.uf}")
-class Endereco:
-    def __init__(self, cep):
-        #Armazena o valor do cep em um link e faz a requisição dele retornando uma resposta
-        cep = cep.replace("-", "").replace(".", "")
-        if len(cep) == 8:
-            link = f"https://viacep.com.br/ws/{cep}/json/"
-            requisicao = requests.get(link) 
+    
+    def escolher_endereco(self, index):
+        return self.enderecos[index]
 
-            dict_requisicao = requisicao.json()
-
-            self.cidade = dict_requisicao['localidade']
-            self.uf = dict_requisicao['uf']
-        else:
-            print("Cep Inválido")
+    def executar_comportamento(self):
+        self.comportamento.executar(self)
+        
+class Vendedor(Usuario):
+    def __init__(self, nome, email, user, telefone, senha):
+        super().__init__(nome, email, user, telefone, senha)
+        self.produtos_cadastrados = []
+    
+    def cadastrar_produtos(self, nome, descricao, preco, tipo, index_endereco):
+        usuario = super().user
+        enderecos = super().enderecos
+        self.produtos_cadastrados.append(Produto(nome, descricao, preco, usuario, tipo, enderecos(index_endereco)))        
